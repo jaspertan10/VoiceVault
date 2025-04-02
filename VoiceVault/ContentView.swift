566c4b894cc1aec535b1818eca1687b73b502dfc
Jasper Tan <jaspertan@Jaspers-MacBook-Air.local>
2025-04-01 23:58:10 -0700

    Add TranscriptionKit package. Enable Start and Stop recordings. Upon stop recordings, audio recording is transcribed to Text

--- VoiceVault/ContentView.swift
+++ VoiceVault/ContentView.swift
@@ -6,16 +6,74 @@
 //
 
 import SwiftUI
+import TranscriptionKit
+
+class History: Identifiable {
+    var id: UUID
+    var date: Date
+    var transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), transcript: String? = nil) {
+        self.id = id
+        self.date = date
+        self.transcript = transcript
+    }
+}
+
+struct RecordingIndicatorView: View {
+    let isRecording: Bool
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text("Recording Status")
+                        .font(.title)
+                    
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
+                        .font(.title)
+                        .padding(.top)
+                }
+
+            }
+    }
+}
 
 struct ContentView: View {
+    
+    @State var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording: Bool = false
+    
+    @State private var history = History()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 40) {
+                
+                if let transcript = history.transcript {
+                    Text(transcript)
+                }
+                
+                Button("Start") {
+                    speechRecognizer.startTranscribing()
+                    isRecording = true
+                }
+                
+                Button("Stop") {
+                    speechRecognizer.stopTranscribing()
+                    isRecording = false
+                    
+                    history.transcript = speechRecognizer.transcript
+                }
+                
+                RecordingIndicatorView(isRecording: isRecording)
+            }
+            .navigationTitle("VoiceVault")
+            .onAppear {
+                speechRecognizer.resetTranscript()
+            }
         }
-        .padding()
     }
 }
 
