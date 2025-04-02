75aa28492acb3cde7d205f29cd257a1c32dedb09
Jasper Tan <jaspertan@Jaspers-MacBook-Air.local>
2025-04-01 21:37:16 -0700

    Initial commit

---
+++ VoiceVault/VoiceVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceVaultApp.swift
+//  VoiceVault
+//
+//  Created by Jasper Tan on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
